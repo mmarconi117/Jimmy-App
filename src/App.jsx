636b352b616274f479c273a29bb25618c2aecbb8
Michael Marconi <michaelziga2@gmail.com>
2024-07-01 16:55:05 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='jim'>
+        <h1>Jimmy's Service</h1>
+
+      </div>
+    </>
+  )
+}
+
+export default App
