3796b0842a9039deafed08eb5cb2d75c28d70dca
Michael Marconi <michaelziga2@gmail.com>
2024-08-02 14:43:43 -0500

    thin header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,10 @@ import './styles/styles.css';
 
 function Header() {
   return (
-    <header className="bg-white-900 bg-opacity-60 text-white py-1 mt-15 overflow-x-hidden w-full">
-      <h1 className="text-orange-500 text-opacity-100 text-left pl-4 text-5xl">Jimmy Filtration Systems</h1>
-    </header>
+    <header className="bg-white-900 bg-opacity-60 text-white py-0 mt-2 overflow-x-hidden w-full">
+  <h1 className="text-orange-500 text-opacity-100 text-left pl-4 text-4xl">Jimmy Filtration Systems</h1>
+</header>
+
   );
 }
 
